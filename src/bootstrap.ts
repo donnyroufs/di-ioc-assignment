b2132f4616d1fee3c92fdbc773b671b7b1aeb4c6
Donny Roufs <iamchets@yahoo.com>
2021-03-28 13:13:54 +0200

    finished example :tada:

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -8,11 +8,38 @@ import { Container } from './container'
 */
 
 // Don't touch the repository!
+//#region Infrastructure
 export class UserRepository {
   public getUsers() {
     return []
   }
 }
 
-export class UserService {}
-export class UserController {}
+export class UserService {
+  constructor(
+    private readonly _userRepository: UserRepository = container.get(
+      UserRepository.name
+    )
+  ) {}
+
+  public getUsers() {
+    return this._userRepository.getUsers()
+  }
+}
+
+export class UserController {
+  constructor(
+    private readonly _userService: UserService = container.get(UserService.name)
+  ) {}
+
+  public index() {
+    return this._userService.getUsers()
+  }
+}
+
+//#endregion
+
+export const container = new Container()
+
+container.bind(UserRepository.name).to(UserRepository)
+container.bind(UserService.name).to(UserService)
--- src/container.ts
+++ src/container.ts
@@ -1,1 +1,18 @@
-export class Container {}
+export class Container {
+  private readonly _deps: Record<string, any> = {}
+  private _key: string = ''
+
+  public bind(key: string) {
+    this._key = key
+    return this
+  }
+
+  public to(dep: any) {
+    this._deps[this._key] = new dep()
+    this._key = ''
+  }
+
+  public get<T>(key: string) {
+    return this._deps[key] as T
+  }
+}
